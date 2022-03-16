3c53021a00fe634e22e59502c0d35531932392e9
Victor Medeiros <victorh.mmedeiros@gmail.com>
2022-03-16 15:27:18 -0400

    Componente ClubesLista

--- parte2_vue/campeonato-brasileiro/src/views/ListaClubes.vue
+++ parte2_vue/campeonato-brasileiro/src/views/ListaClubes.vue
@@ -1,35 +1,17 @@
 <template>
   <v-container>
     <h2 class="text-h5 text-center mb-3 mt-5">Tabela de classificação</h2>
-    <v-simple-table>
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th colspan="2" class="text-left">Clube</th>
-          <th class="text-right">Pontos</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(clube, index) of listaClubes" :key="clube.id">
-          <td>{{ index + 1 }}</td>
-          <td>
-            <v-avatar size="24">
-              <img :src="clube.escudo" :alt="clube.nome" />
-            </v-avatar>
-            <span class="pl-2">{{ clube.nome }}</span>
-          </td>
-          <td class="text-right">{{ clube.pontos }}</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+    <TabelaClubes :clubes="listaClubes"/>
   </v-container>
 </template>
 
 <script>
-
+import TabelaClubes from '../components/TabelaClubes.vue'
 export default {
   name: 'ListaClubes',
+  components: {
+    TabelaClubes
+  },
   data() {
     return {
       listaClubes: [],
