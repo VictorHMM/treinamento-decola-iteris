2cd88d5a74d4d14684fe5648a214bc59d3fb7bb4
Victor Medeiros <victorh.mmedeiros@gmail.com>
2022-03-17 07:45:19 -0400

    Primeiro commit todo list

---
+++ projeto/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app id="inspire">
+    <AppTopBar/>
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppTopBar from './components/shared/AppTopBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppTopBar
+  },
+};
+</script>
---
+++ projeto/src/components/shared/AppTopBar.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container>
+    <v-app-bar
+      app
+      prominent
+      color="#fcb69f"
+      dark
+      src="./../../../public/river.jpg"
+      scroll-target="#scrolling-techniques-2"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
+      <v-navigation-drawer
+      v-model="drawer"
+      app
+      temporary
+    >
+    <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-format-list-checks</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Your list</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-information-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      <!--  -->
+    </v-navigation-drawer>
+
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Todo list</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "AppTopBar",
+
+  data: () => ({
+      drawer: false,
+      group: null,
+    }),
+};
+</script>
+
+<style scoped></style>
