75fa948b4956812d55d1a77ec0f5cce2c344ca87
Victor Medeiros <victorh.mmedeiros@gmail.com>
2022-03-18 14:28:33 -0400

    Countries sorted

--- paises/src/components/shared/AppTopBar.vue
+++ paises/src/components/shared/AppTopBar.vue
@@ -1,4 +1,5 @@
 <template>
+<v-container>
     <v-app-bar
       absolute
       color="#fcb69f"
@@ -7,25 +8,58 @@
       src="../../assets/map.png"
       scroll-target="#scrolling-techniques-2"
     >
-      <v-app-bar-nav-icon color="black"></v-app-bar-nav-icon>
-
-      <v-app-bar-title id="title">Your country</v-app-bar-title>
-
-      <v-spacer></v-spacer>
 
       <v-btn icon>
         <v-icon color="black">mdi-magnify</v-icon>
       </v-btn>
 
-      <v-btn icon>
-        <v-icon color="black">mdi-dots-vertical</v-icon>
-      </v-btn>
+      <v-app-bar-title id="title">Your country</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+    <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+            color="black"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+    
+        <v-list>
+            <span class="ml-3">Sort by:</span>
+          <v-list-item
+            @click="() => {}"
+          >
+            <v-list-item-title>Name</v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            @click="() => {}"
+          >
+            <v-list-item-title>Capital</v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            @click="() => {}"
+          >
+            <v-list-item-title>Population</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
+    </v-container>
 </template>
 
 <script>
 export default {
     name: 'AppTopBar',
+    data: () => ({
+      
+    }),
 }
 </script>
 
